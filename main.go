f21d75aa3ef0676bd422d775996ce52cc839439e
Michael J. Roberts <michael@observerly.com>
2022-04-22 16:45:39 +0100

    feat: Added default r.GET() to the index route.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "Welcome to the Nocturnal 🌑 API"})
+	})
+
+	r.Run(":3000")
+}
